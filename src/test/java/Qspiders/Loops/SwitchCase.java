e13428a92c5063bd889c04012c3e95e1bc6fb98c
prashantsj <prashantsj82@gmail.com>
2021-10-10 16:12:29 +0530

    first commit

---
+++ src/test/java/Qspiders/Loops/SwitchCase.java
@@ -0,0 +1,8 @@
+package Qspiders.Loops;
+
+public class SwitchCase {
+
+    public static void main(){
+        System.out.println("HI");
+    }
+}
---
+++ src/test/java/Qspiders/Loops/SwitchCase2.java
@@ -0,0 +1,8 @@
+package Qspiders.Loops;
+
+public class SwitchCase2 {
+
+    public static void main(){
+        System.out.println("HI");
+    }
+}
